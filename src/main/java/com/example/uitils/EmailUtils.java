ce53140fc111c393f421b1e8f61870c058449702
小黑 <76620834+xiaohei-maker@users.noreply.github.com>
2021-09-10 10:14:24 +0800

    Update EmailUtils.java

--- src/main/java/com/example/uitils/EmailUtils.java
+++ src/main/java/com/example/uitils/EmailUtils.java
@@ -76,8 +76,8 @@ public class EmailUtils {
 			message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(user.getEmail(), user.getName(), "utf-8"));
 			//3.3生成邮件主题
 			message.setSubject("小黑社区账号激活邮件","utf-8");
-			//String ip = Inet4Address.getLocalHost().getHostAddress();
-			String url = "http://110.42.132.132:80/jihuo&c="+Base64Utils.encode(user.getCode());
+			String ip = Inet4Address.getLocalHost().getHostAddress();
+			String url = "http://"+ip+":80/jihuo&c="+Base64Utils.encode(user.getCode());
 			//设置邮件正文 setContent 可以使用html标签
 			message.setContent(user.getName()+",你好<br>欢迎注册小黑社区! 请点击链接进行激活:<a href='"+url+"'>点击此处</a>","text/html;charset=utf-8");
 			//设置邮件的发送时间 是立即发送
